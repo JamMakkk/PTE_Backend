a56641ce723e977736d8c2ddba3481379107ef82
JamMakkk <87015142+JamMakkk@users.noreply.github.com>
2026-02-03 14:31:07 +1100

    Update

---
+++ Shared/FileHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Shared
+{
+    public class FileHandler
+    {
+        public static async Task<bool> SaveUploadedFile(Stream file, string filePath) 
+        {
+            await using var stream = new FileStream(
+                filePath,
+                FileMode.Create,
+                FileAccess.Write,
+                FileShare.None,
+                bufferSize: 81920,
+                useAsync: true
+            );
+
+            await file.CopyToAsync(stream);
+            return true;
+        }
+    }
+}
